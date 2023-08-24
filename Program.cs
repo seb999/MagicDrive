fe7061b850f6934daf457178bacd6e508f7b51f8
sdubos <sebastien.dubos@ecdc.europa.eu>
2023-08-24 20:52:29 +0200

    Updates from PC

--- Program.cs
+++ Program.cs
@@ -3,93 +3,163 @@ using MagicDrive.Misc;
 using MagicDrive.MLClass;
 using MMALSharp;
 using MMALSharp.Common;
-using MMALSharp.Components;
 using MMALSharp.Handlers;
-using MMALSharp.Native;
-using MMALSharp.Ports;
-using MMALSharp.Ports.Outputs;
 
 namespace MagicDrive
 {
     internal class Program
     {
 
-        //to be defined
+        // PIN / Variables
         private static string pictureFolderPath = @"home/Pictures";
-        private static int STANDBY_LED = 23;
-        private static int DRIVE_LED = 24;
-        private static int START_BTN = 18;
-
-        private static FileSystemWatcher? watcher;
-        private static MMALCamera cam = MMALCamera.Instance;
         private static bool isAutoDrive = false;
-        private static StepperMotorController? motor;
-        private static LedController? standByLed;
-        private static LedController? driveLed;
+        private static bool isTrainModel = false;
+        private static int LED_STANDBY = 23;
+        private static int LED_DRIVING = 24;
+        private static int BTN1 = 18;
+        private static int BTN2 = 17;
+        private static int BTN3 = 27;
+        private static int BTN4 = 22;
+
+        // Led
+        private static LedController? ledStandBy;
+        private static LedController? ledDriving;
+
+        // Switch
         private static SwitchController? startStopButton;
+        private static SwitchController? leftButton;
+        private static SwitchController? rightButton;
+        private static SwitchController? centerButton;
+
+        //other
+        private static MMALCamera cam = MMALCamera.Instance;
+        private static StepperMotorController? motor;
         private static FileWatcher? fileWatcher;
 
         private static void Main(string[] args)
         {
-            // 0 - Uncomment when all part tested
-            // motor = new StepperMotorController();
-            // standByLed = new LedController(standbyLedGPIO);
-            // magicDriveLed = new LedController(magicDriveLedGPIO);
-            // startStopButton = new SwitchController(START_BTN,  OnButtonPinValueChanged);
-            // fileWatcher = new FileWatcher(pictureFolderPath, OnFileCreated); 
-            // magicDriveLed.Off();
-            // standByLed.On();
+            motor = new StepperMotorController();
+            ledStandBy = new LedController(LED_STANDBY);
+            ledDriving = new LedController(LED_DRIVING);
+            startStopButton = new SwitchController(BTN1, OnBtnStartStopChanged);
+            leftButton = new SwitchController(BTN2, OnBtnLeftChanged);
+            centerButton = new SwitchController(BTN3, OnBtnCenterChanged);
+            rightButton = new SwitchController(BTN4, OnBtnRightChanged);
 
-            ///////////////////////////
-            //      Debug      ////////
-            ///////////////////////////
+            fileWatcher = new FileWatcher(pictureFolderPath, OnFileCreated);
+            ledDriving.Off();
+            ledStandBy.On();
+
+            Debug();
+
+            //2 - Train model
+            // isTrainModel = true;
+        }
+
+        #region Degugging
+
+        private static void Debug()
+        {
             //Test leds and motor and comment out
-            standByLed = new LedController(STANDBY_LED);
-            driveLed = new LedController(DRIVE_LED);
+            ledStandBy = new LedController(LED_STANDBY);
+            ledDriving = new LedController(LED_DRIVING);
             motor = new StepperMotorController();
 
             while (1 == 1)
             {
-                standByLed.Blink();
-                standByLed.Blink();
-                standByLed.Blink();
-                driveLed.Blink();
-                driveLed.Blink();
+                ledStandBy.Blink();
+                ledStandBy.Blink();
+                ledStandBy.Blink();
+                ledDriving.Blink();
+                ledDriving.Blink();
                 Thread.Sleep(1000);
                 motor.TurnLeft(100);
                 Thread.Sleep(1000);
                 motor.TurnRight(1000);
             }
 
-            //Test event handler on new file
-            //fileWatcher = new FileWatcher(pictureFolderPath, OnFileCreated); 
-
-            //Test button active on / off
-            //startStopButton = new SwitchController(START_BTN,  OnButtonPinValueChanged);
-
             //Test take picture
             //TakePicture();
+
+            //Collect picture to train model
+            // isCollectPicture = true;
         }
 
-        private static void OnButtonPinValueChanged(object sender, PinValueChangedEventArgs e)
+        #endregion
+
+        #region button event handler
+
+        private static void OnBtnLeftChanged(object sender, PinValueChangedEventArgs e)
+        {
+            if (e.ChangeType == PinEventTypes.Falling)
+            {
+                if (isTrainModel)
+                {
+                    TakePicture("Left");
+                }
+                else
+                {
+                    //Stop AI and drive to left
+                    isAutoDrive = false;
+                    ledStandBy?.On();
+                    ledDriving?.Off();
+                    fileWatcher?.Stop();
+                    motor.TurnLeft(50);
+                }
+            }
+        }
+
+        private static void OnBtnRightChanged(object sender, PinValueChangedEventArgs e)
+        {
+            if (isTrainModel)
+                {
+                    TakePicture("Right");
+                }
+                else
+                {
+                    //Stop AI and drive to left
+                    isAutoDrive = false;
+                    ledStandBy?.On();
+                    ledDriving?.Off();
+                    fileWatcher?.Stop();
+                    motor.TurnRight(50);
+                }
+        }
+
+        private static void OnBtnCenterChanged(object sender, PinValueChangedEventArgs e)
+        {
+           if (isTrainModel)
+                {
+                    TakePicture("Center");
+                }
+                else
+                {
+                    //Stop AI and drive to left
+                    isAutoDrive = true;
+                    ledStandBy?.Off();
+                    ledDriving?.On();
+                    fileWatcher?.Start();
+                }
+        }
+        private static void OnBtnStartStopChanged(object sender, PinValueChangedEventArgs e)
         {
             if (e.ChangeType == PinEventTypes.Falling)
             {
                 if (!isAutoDrive)
                 {
-                     isAutoDrive = true;
+                    isAutoDrive = true;
 
                     ///////////////////////////
                     //      Debug      ////////
                     ///////////////////////////
-                    standByLed.Blink();
-                    standByLed.Blink();
-                    standByLed.Blink();
+                    ledStandBy.Blink();
+                    ledStandBy.Blink();
+                    ledStandBy.Blink();
 
                     // standByLed?.Off();
                     // driveLed?.On();
                     // fileWatcher.Start();
-                    // TakePicture();
+                    // TakePictureTimeout();
                 }
                 else
                 {
@@ -98,8 +168,8 @@ namespace MagicDrive
                     ///////////////////////////
                     //      Debug      ////////
                     ///////////////////////////
-                    driveLed.Blink();
-                    driveLed.Blink();
+                    ledDriving.Blink();
+                    ledDriving.Blink();
 
                     // standByLed?.On();
                     // driveLed?.Off();
@@ -109,28 +179,39 @@ namespace MagicDrive
             }
         }
 
+        #endregion
+
+        #region files event handler
+
         private static void OnFileCreated(object sender, FileSystemEventArgs e)
         {
-            fileWatcher?.Stop();
+            if (!isTrainModel)
+            {
+                fileWatcher?.Stop();
 
-            ///////////////////////////
-            //      Debug      ////////
-            ///////////////////////////
-            standByLed.Blink();
-            standByLed.Blink();
-            standByLed.Blink();
-            driveLed.Blink();
-            driveLed.Blink();
+                ///////////////////////////
+                //      Debug      ////////
+                ///////////////////////////
+                ledStandBy.Blink();
+                ledStandBy.Blink();
+                ledStandBy.Blink();
+                ledDriving.Blink();
+                ledDriving.Blink();
 
-            //Comment out when debugging completed
-            //ProcessImage(e.FullPath);
+                //Comment out when debugging completed
+                //ProcessImage(e.FullPath);
 
-            CleanImageFolder();
+                CleanImageFolder();
 
-            fileWatcher?.Start();
+                fileWatcher?.Start();
+            }
         }
 
-        private static async void TakePicture()
+        #endregion
+
+        #region Camera and AI processing
+
+        private static async void TakePicturTimeout()
         {
             using (var imgCaptureHandler = new ImageStreamCaptureHandler(pictureFolderPath, "jpg"))
             {
@@ -139,6 +220,19 @@ namespace MagicDrive
                 await cam.TakePictureTimeout(imgCaptureHandler, MMALEncoding.JPEG, MMALEncoding.I420, cts.Token);
             }
 
+            // Only call when you no longer require the camera, i.e. on app shutdown.
+            cam.Cleanup();
+        }
+
+        private static async void TakePicture(string folder)
+        {
+            using (var imgCaptureHandler = new ImageStreamCaptureHandler(pictureFolderPath, "jpg"))
+            {
+                await cam.TakePicture(imgCaptureHandler, MMALEncoding.JPEG, MMALEncoding.I420);
+                var source = imgCaptureHandler.GetFilepath();
+                var destination = $"{pictureFolderPath}/{folder}";
+                File.Move(source, destination);
+            }
             // Only call when you no longer require the camera, i.e. on app shutdown.
             cam.Cleanup();
         }
@@ -176,5 +270,7 @@ namespace MagicDrive
                 file.Delete();
             }
         }
+
+        #endregion
     }
 }
